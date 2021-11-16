dada60127d5437d5a12116520eaa0e1de8845541
Yuseok Jeon <ysjeon@unist.ac.kr>
2021-11-16 15:22:18 +0900

    fix outdated llvm git repo link

--- scripts/get_llvm_src_tree.sh
+++ scripts/get_llvm_src_tree.sh
@@ -1,19 +1,19 @@
 #!/bin/bash -e
 
 #get LLVM
-git clone http://llvm.org/git/llvm.git
+git clone git@github.com:llvm-mirror/llvm.git
 pushd llvm
 git reset --hard 8f4f26c9c7fec12eb039be6b20313a51417c97bb
 popd
 
 #get clang
-git clone http://llvm.org/git/clang.git
+git clone git@github.com:llvm-mirror/clang.git
 pushd clang
 git reset --hard d59a142ef50bf041797143db71d2d4777fd32d27
 popd
 
 #get compiler-rt
-git clone http://llvm.org/git/compiler-rt.git
+git clone git@github.com:llvm-mirror/compiler-rt.git
 pushd compiler-rt
 git reset --hard 961e78720a32929d7e4fc13a72d7266d59672c42
 popd
